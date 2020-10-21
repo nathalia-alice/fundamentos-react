e0514ed6554ae4775ebfd71c98e036fe0189193b
nathalia-alice <nathalia.alice.silv@gmail.com>
2020-10-20 22:40:50 -0300

    Contador #01

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import React from 'react'
 
+import Contador from './components/contador/Contador'
 import Input from './components/formulario/Input'
 import IndiretaPai from "./components/comunicacao/IndiretaPai"
 import DiretaPai from "./components/comunicacao/DiretaPai"
@@ -21,7 +22,11 @@ export default () => (
         <h1>Fundamentos React (Arrow)</h1>
 
         <div className="Cards">
-            <Card titulo="#11 - Componente Controlado (Input" color="#E45F56">
+            <Card titulo="#12 - Contador" color="#424242">
+                <Contador numeroInicial={10}></Contador>
+            </Card>
+
+            <Card titulo="#11 - Componente Controlado (Input)" color="#E45F56">
                 <Input></Input>
             </Card>
 
